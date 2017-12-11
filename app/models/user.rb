97345dd34a50f129d02215c467ae0dcc01fa73d2
Colin Richardson <richardsonjcolin@gmail.com>
2017-12-11 23:49:18 +0000

    update login method to simplify testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ActiveRecord::Base
-  validates :email, uniqueness: true
-
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
+  validates :password_confirmation, length: { minimum: 8 }
+  before_save :email_lowercase
+
+  def email_lowercase
+    self.email.downcase!
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 
 end
